1f99fb509e0040a63d6bec4c143e56a7ea029a7b
Nguyễn Lý Nhật Phương <57012228+nhatphuong99@users.noreply.github.com>
2021-06-13 04:04:16 +0700

    Adjust valid step

--- codes/run.py
+++ codes/run.py
@@ -62,7 +62,7 @@ def parse_args(args=None):
     parser.add_argument('--warm_up_steps', default=None, type=int)
     
     parser.add_argument('--save_checkpoint_steps', default=10000, type=int)
-    parser.add_argument('--valid_steps', default=10000, type=int)
+    parser.add_argument('--valid_steps', default=100, type=int)
     parser.add_argument('--log_steps', default=100, type=int, help='train log every xx steps')
     parser.add_argument('--test_log_steps', default=1000, type=int, help='valid/test log every xx steps')
     
